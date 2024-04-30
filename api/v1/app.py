42942c38302b0555b6c9868053b8146f81a7de2f
Judith Ukachukwu <ukachukwujudith95@gmail.com>
2024-04-30 19:52:50 +0100

    create a bluueprint and return an ok status code

---
+++ api/v1/app.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" build an api """
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+
+host = getenv('HBNB_API_HOST')
+port = getenv('HBNB_API_PORT')
+
+app = Flask(__name__)
+app.register_blueprint(app_views, url_prefix='/api/v1')
+
+
+@app.teardown_appcontext
+def close_session(exception):
+    """ closes a sesion """
+    storage.close()
+
+
+if __name__ == '__main__':
+    if host:
+        valid_host = host
+    else:
+        valid_host = '0.0.0.0'
+    if port:
+        valid_port = port
+    else:
+        valid_port = 5000
+    app.run(host=valid_host, port=valid_port, threaded=True)
