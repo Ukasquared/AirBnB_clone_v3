52f97deea19e21ad3068aa6c729691f41441bb88
Judith Ukachukwu <ukachukwujudith95@gmail.com>
2024-05-01 14:28:28 +0100

    fetch the number of state in a database

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,7 +2,14 @@
 """ index module """
 from flask import jsonify
 from api.v1.views import app_views
-
+from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models import storage
 
 @app_views.route('/status', strict_slashes=False, methods=['GET'])
 def status():
@@ -10,4 +17,16 @@ def status():
     response = {"status": "OK"}
     return jsonify(response)
 
-
+@app_views.route('/stats', strict_slashes=False, methods=['GET'])
+def stats():
+    """ returns statistics of classes"""
+    classes = {"amenities": Amenity,
+               "cities": City,
+               "places": Place,
+               "reviews": Review,
+               "states": State,
+               "users": User}
+    cls_count = {}
+    for key, value in classes.items():
+        cls_count[key] = storage.count(value)
+    return (cls_count)
