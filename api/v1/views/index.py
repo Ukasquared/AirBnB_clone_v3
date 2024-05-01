31e4b329073c79a2ebbde4a76c353f07cc105275
Judith Ukachukwu <ukachukwujudith95@gmail.com>
2024-05-01 04:51:48 +0100

    adjustment

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,7 +3,7 @@ from flask import jsonify
 from api.v1.views import app_views
 
 
-@app_views.route('/status', methods=['GET'])
+@app_views.route('/status', strict_slashes=False, methods=['GET'])
 def status():
     """ returns response """
     response = {"status": "OK"}
