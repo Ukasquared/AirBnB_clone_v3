2ea523ddd018dc1ea081af5d854480600a1c30f9
Judith Ukachukwu <ukachukwujudith95@gmail.com>
2024-05-03 03:34:44 +0100

    buid an api for cities

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -7,16 +7,18 @@ from models.state import State
 from api.v1.views import app_views
 from flask import jsonify, request, abort
 
-@app_views.route('/api/v1/states/<state_id>/cities', strict_slashes=False, methods=['GET'] )
+
+@app_views.route('/states/<state_id>/cities',
+                 strict_slashes=False, methods=['GET'])
 def list_cities(state_id):
     """ lists all the cities
     in the database """
     # return all states from storage
-    for state_id, state in storage.all(State).items():
-        id_state = state_id.split('.')[1]
+    for statet_id, state in storage.all(State).items():
+        id_state = statet_id.split('.')[1]
         if id_state == state_id:
             for city in storage.all(City).values():
-                if city.State_id == state_id:
+                if city.state_id == state_id:
                     return jsonify(city.to_dict())
     abort(404)
 
@@ -30,18 +32,20 @@ def list_city_id(city_id):
         abort(404, description='handle_error')
     return jsonify(city_w_id.to_dict())
 
-@app_views.route('/cities/<city_id>', strict_slashes=False, methods=['DELETE'])
-def delete_state(city_id):
-    """ delete city id """
-    for city_id, city in storage.all(City).items():
-        id_city = city_id.split('.')[1]
-        if id_city == city_id:
-            storage.delete(city)
-            storage.save()
-            return (jsonify("{}"), 200)
-    abort(404)
 
-@app_views.route('/states/<state_id>/cities', strict_slashes=False, methods=['POST'])
+@app_views.route('/cities/<city_id>', strict_slashes=False, methods=['DELETE'])
+def delete_city(city_id):
+    """ delete city id """
+    city = storage.get(City, city_id)
+    if city is None:
+        abort(404)
+    storage.delete(city)
+    storage.save()
+    return (jsonify("{}"), 200)
+
+
+@app_views.route('/states/<state_id>/cities', strict_slashes=False,
+                 methods=['POST'])
 def post_city_in_state(state_id):
     """ link city to state using
     state id """
@@ -49,7 +53,7 @@ def post_city_in_state(state_id):
     state = storage.get(State, state_id)
     if state.id != state_id:
         abort(404)
-    if city == None:
+    if city is None:
         abort("Not a JSON", 400)
     if 'name' not in city:
         abort("Missing name", 400)
@@ -70,6 +74,7 @@ def update_city(city_id):
         if id_citie == city_id:
             for key, val in upd_city.items():
                 if key not in ['id', 'created_at', 'updated_at']:
-                    setattr(state, key, val)
+                    setattr(city, key, val)
+            city.save()
             return jsonify(city.to_dict()), 200
     return abort(404)
