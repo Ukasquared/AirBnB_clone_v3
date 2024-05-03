401b5a33337fbdb7417949ee4362be003ad9d4d4
Judith Ukachukwu <ukachukwujudith95@gmail.com>
2024-05-03 01:31:10 +0100

    build an api for state

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -4,7 +4,7 @@ RESTFUL API actions """
 from models import storage
 from models.state import State
 from api.v1.views import app_views
-from flask import jsonify
+from flask import jsonify, request, abort
 
 @app_views.route('/states', strict_slashes=False, methods=['GET'] )
 def list_states():
@@ -27,7 +27,45 @@ def list_states_id(state_id):
         abort(404, description='handle_error')
     return jsonify(state_w_id.to_dict())
 
-# @app_views.route('states/<state_id>', strict_slashes=False, method=['DELETE'])
-#def delete_state(state_id):
-#   """ delete state id """
 
+@app_views.route('states/<state_id>', strict_slashes=False, methods=['DELETE'])
+def delete_state(state_id):
+    """ delete state id """
+    for states_key, state in storage.all(State).items():
+        id_state = states_key.split('.')[1]
+        if id_state == state_id:
+            state.delete()
+            return ("{}", 200)
+        else:
+            abort(404, description='handle_error')
+
+
+@app_views.route('/states', strict_slashes=False, methods=['POST'])
+def post_state():
+    """ add a state to database """
+    state = request.get_json()
+    if state == None:
+        abort("Not a JSON", 400)
+    if not 'name' in state:
+        abort('Missing name', 404)
+    # create new state
+    new_state = State(**state)
+    new_state.save()
+    return (new_state.to_dict(), 201)
+
+
+@app_views.route('/states/<state_id>', strict_slashes=False, methods=['PUT'])
+def put_state(state_id):
+    """ updates the state object
+    in the database """
+    upd_state = request.get_json()
+    if upd_state is None:
+        abort("Not a JSON", 400)
+    for statet_id, state in storage.all(State).items():
+        id_state = statet_id.split('.')[1]
+        if id_state == state_id:
+            for key, val in upd_state.items():
+                if key not in ['id', 'created_at', 'updated_at']:
+                    setattr(state, key, val)
+            return (state.to_dict(), 200)
+    return abort(404)
